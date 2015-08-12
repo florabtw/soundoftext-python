3ffc7489358aac33df097f87cdc4e6dc341ce3da
Nick Pierson <nkcrpn@gmail.com>
2015-08-11 19:12:29 -0500

    Barebones flask project

---
+++ soundoftext.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def index():
+    return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run()
