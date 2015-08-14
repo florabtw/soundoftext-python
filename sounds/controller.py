237a2f2d2c3c0836b4c7464592d4b1895ac5db40
Nick Pierson <nkcrpn@gmail.com>
2015-08-13 19:34:22 -0500

    Standardize input text: trimmed, lowercase, and 100 chars or less

--- sounds/controller.py
+++ sounds/controller.py
@@ -17,7 +17,7 @@ s = requests.Session()
 
 def create():
     lang = request.form['lang']
-    text = request.form['text']
+    text = request.form['text'].strip().lower()[:100]
 
     if sound_exists(lang, text):
         sound = get_sound_by_lang_text_pair(lang, text)
